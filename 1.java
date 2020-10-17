8f40758280dd603ed1a439cc42769056e854b262
bmoczt <56966103+robin9877@users.noreply.github.com>
2020-10-17 11:04:57 +0800

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,10 @@
+public class lpp{
+    public static void main(String[] args){
+       int x = 5;
+       int y;
+       y = x ;
+       x = 2;
+       System.out.println("x is :"+x);
+       System.out.println("y is :"+y);
+    }
+}
