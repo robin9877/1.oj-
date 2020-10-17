70000caaaba521c6a096aff2523140744eddc99f
bmoczt <56966103+robin9877@users.noreply.github.com>
2020-10-17 11:41:28 +0800

    Add files via upload

---
+++ 2.sumtwo.py
@@ -0,0 +1,13 @@
+num1=input("请输入第一个数字：")
+num2=input("请输入第二个数字：")
+sum = float(num1)+float(num2)
+print("数字{0}和{1}相加等于:{2}".format(num1,num2,sum))
+"""
+num1 = input('输入第一个数字：')
+num2 = input('输入第二个数字：')
+ 
+# 求和
+sum = float(num1) + float(num2)
+ 
+# 显示计算结果
+print('数字 {0} 和 {1} 相加结果为： {2}'.format(num1, num2, sum))"""
