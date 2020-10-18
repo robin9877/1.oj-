4039409957dea0d869b701720b9374c5af7fc99e
bmoczt <56966103+robin9877@users.noreply.github.com>
2020-10-18 21:55:37 +0800

    Update 1.java

--- 1.java
+++ 1.java
@@ -8,3 +8,36 @@ public class lpp{
        System.out.println("y is :"+y);
     }
 }
+
+
+/*** 
+public class lpp{
+    public static void main(String[] args){
+       int x = 5;
+       int y;
+       y = x ;
+       x = 2;
+       System.out.println("x is :"+x);
+       System.out.println("y is :"+y);
+    }
+}
+***//*** 
+public class lpp{
+
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}**/
+/*** 
+public class lpp{
+    public static void main(String[] args){
+        String strOrig ="hello world,hello Runoob";
+        int lastIndex = strOrig.lastIndexOf("Runoob");
+        if(lastIndex == -1){
+            System.out.println("没有找到字符串 Runoob");
+        }else{
+            System.out.println("Runoob 字符串最后出现的位置： "+lastIndex);
+        }
+    }
+}
+***/
